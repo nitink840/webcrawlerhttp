317a901089f80c5d4df9595f413a7a6e38010799
nitink840 <nitin.kushwaha120@gmail.com>
2023-02-11 20:48:51 +0530

    made crawl page and editted main func.

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("hello world!");
+const {crawlPage} =  require('./src/crawl');
+async function main() {
+    if(process.argv.length <3){
+        console.log("No argument provided!"); 
+        process.exit(1)   
+    }
+    if(process.argv.length >3){
+        console.log("Too many argument provided!"); 
+        process.exit(1)   
+    }
+    const baseUrl = process.argv[2]
+    console.log(`Started Crawling ${baseUrl}...`)
+
+    const pages = await crawlPage(baseUrl,baseUrl,{});
+    for (const page of Object.entries(pages)) {
+        console.log(page)
+    }
+}
+
+main();
--- src/crawl.js
+++ src/crawl.js
@@ -1,9 +1,53 @@
 const {JSDOM} = require('jsdom');
+let count =0;
+ async function crawlPage(baseUrl,currentUrl,pages) {
+    const baseUrlObj = new URL(baseUrl);
+    const currentUrlObj = new URL(currentUrl);
+    if(baseUrlObj.hostname !== currentUrlObj.hostname){
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentUrl);
+    if(pages[normalizedCurrentURL] > 0){
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+    
+    pages[normalizedCurrentURL] = 1
+
+    console.log(`Actively crawling ${currentUrl}`)
+    // let htmlBody='';
+    try {
+        const resp = await fetch(currentUrl);
+
+        if(resp.status > 399){
+            console.log(`error in fetch with status code ${resp.status} on page ${currentUrl}`)
+            return pages
+        }
+
+        const contentType = resp.headers.get('content-type');
+        if(!contentType.includes("text/html")){
+            console.log(`non-html response, content type: ${contentType} on page ${currentUrl}`)
+            return pages
+        }
+
+        const htmlBody = await resp.text()
+        const nextUrls = urlFromHtml(htmlBody,baseUrl)
+        for (const nextUrl of nextUrls) {
+            pages = await crawlPage(baseUrl,nextUrl,pages)
+        }
+        
+    }catch (error) {
+        console.log(`Error in fetch: ${error.message} on page: ${currentUrl}`)
+    }
+    
+    return pages
+}
 
 function urlFromHtml(htmlBody,baseUrl) {
+    const urls=[];
     const dom = new JSDOM(htmlBody);
     const links = dom.window.document.querySelectorAll("a");
-    const urls=[];
     for (const link of links) {
         if(link.href.startsWith('/')){
             try {
@@ -35,5 +79,6 @@ function normalizeURL(urlString) {
 // console.log(normalizeURL('https://boot.dev/path/'));
 module.exports={
     normalizeURL,
-    urlFromHtml
+    urlFromHtml,
+    crawlPage
 }
